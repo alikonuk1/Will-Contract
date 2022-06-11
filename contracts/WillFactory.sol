d896728e72b5df38247fa2906da17373416eb754
ali konuk <alikonuk1@gmail.com>
2022-06-11 16:11:02 +0300

    contract upgrade

--- contracts/WillFactory.sol
+++ contracts/WillFactory.sol
@@ -20,10 +20,25 @@ contract WillFactory {
 
 contract Will {
     address public willUser;
+    address public builderDao = 0x0000000000000000000000000000000000000001;
     uint256 public expiration;
     address[] private _guardians;
+    
+    struct guardianData {
+        bool isGuardian; 
+    }
 
+    mapping(address => guardianData) public guardianInfo;
 
+    modifier onlyOwner() {
+        require (msg.sender == willUser, "Unauthorized");
+        _;
+    }
+
+    modifier expiredCheck() {
+        require (isExpired() == false, "Already Expired");
+        _;
+    }
 
     constructor(address _willUser, uint256 _expiration) {
         willUser = _willUser;
@@ -36,22 +51,24 @@ contract Will {
     }
 
     // Set more time.
-    function setExtension(uint256 _extension) external {
-        expiration = block.timestamp + _extension;
+    function setExtension(uint256 _extension) external onlyOwner expiredCheck {
+        expiration = block.timestamp + timeLeft() + _extension;
     }
     
     // How much time left.
-    function timeLeft() public view returns (uint256) {
+    function timeLeft() public view onlyOwner returns (uint256) {
         return expiration - block.timestamp;
     }
 
     // Set guardians who will recieve the will after expiration.
-    function setGuardian(address _guardian) external {
+    function setGuardian(address _guardian) external onlyOwner expiredCheck {
+        guardianData storage _guardianA = guardianInfo[_guardian];
         _guardians.push(_guardian);
+        _guardianA.isGuardian = true;
     }
 
     // Check the guardians.
-    function getGuardians() external view returns (address[] memory) {
+    function getGuardians() external view onlyOwner returns (address[] memory) {
         return _guardians;
     }
 
@@ -60,27 +77,38 @@ contract Will {
         console.log("contract balance:", address(willUser).balance);
         return address(willUser).balance;
     }
-/*
-    function finalizeWill() private {
-//        uint commissionAmount = address(willUser).balance * 2 / 100;
-//        payCommission(commissionAmount);
-        sendAssets(guardians);
-        //getBalance();
+
+    // ***
+    function ownershipTransfer(address _guardian, address superGuardian) external {
+        guardianData storage _guardianA = guardianInfo[_guardian];
+        require (isExpired() == true, "Not Expired");
+        require(_guardianA.isGuardian, "Not Guardian");
+        superGuardian = _guardian;
+        finalizeWill(superGuardian);
+    }
+
+    // ***
+    function finalizeWill(address superGuardian) private {
+//        require (isExpired() == true, "Not Expired");
+        require (msg.sender == superGuardian, "Not guardian");
+        uint commissionAmount = address(willUser).balance * 2 / 100;
+        payCommission(commissionAmount);
+        sendAssets(superGuardian);
+        getBalance();
     }
 
     // 
-    function sendAssets() private {
-        console.log("Sending guardians the balance of willUser:", address(willUser).balance);
-        (bool sent, bytes memory data) = guardians.call{value: address(willUser).balance}("");
+    function sendAssets(address superGuardian) private {
+        console.log("Sending willUser balance to guardians:", address(willUser).balance);
+        (bool sent, bytes memory data) = superGuardian.call{value: address(willUser).balance}("");
         require(sent, "Failed to send Ether");
-        //getBalance();
+        getBalance();
     }
-/*
+
     function payCommission(uint commissionAmount) private {     
         console.log("Paying the commission of amount %s ", commissionAmount);
-        (bool sent, bytes memory data) = alikonuk.call{value: commissionAmount}("");
+        (bool sent, bytes memory data) = builderDao.call{value: commissionAmount}("");
         require(sent, "Failed to send Ether");      
-        //getBalance(); 
+        getBalance(); 
     }
-*/
 }
