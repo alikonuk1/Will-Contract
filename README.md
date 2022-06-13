96a36d22c0ad9e0662c6ec930fe1b92ad8f21543
ali konuk <alikonuk1@gmail.com>
2022-06-14 01:05:09 +0300

    foundry support added

--- README.md
+++ README.md
@@ -1,15 +1,74 @@
-# Basic Sample Hardhat Project
+# Setup
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+You'll need the following:
 
-Try running some of the following tasks:
+- `RPC URL`: A URL to connect to the blockchain. You can get one for free from [Ankr](https://www.ankr.com/protocol/).
+- `PRIVATE_KEY`: A private key from your wallet. You can get a private key from a new [Metamask](https://metamask.io/) account.
+- `API_KEY`: A key to to verify our contract. You can get one from a desired chains block explorer.
 
 ```shell
-npx hardhat accounts
+npm install
+```
+
+or
+
+```shell
+yarn
+```
+
+# Compile
+
+## Foundry
+
+```shell
+forge build
+```
+
+## Hardhat
+
+```shell
 npx hardhat compile
-npx hardhat clean
+```
+
+# Test
+
+## Foundry
+
+```shell
+forge test
+```
+
+## Hardhat
+
+```shell
 npx hardhat test
-npx hardhat node
-node scripts/sample-script.js
-npx hardhat help
+```
+
+# Deploy
+
+## Foundry
+
+```shell
+forge create --rpc-url <your_rpc_url> --private-key <your_private_key> src/WillFactory.sol:WillFactory
+```
+
+## Hardhat
+
+```shell
+npx hardhat run scripts/deploy.js --network mumbai
+```
+
+# Verify
+
+## Foundry
+
+```shell
+# for chain id = https://evm-chainlist.netlify.app/
+forge verify-contract --chain-id 80001 CONTRACT_ADDRESS src/WillFactory.sol:WillFactory API_KEY
+```
+
+## Hardhat
+
+```shell
+npx hardhat verify <deployed_contracts_address> --contract contracts/WillFactory.sol:WillFactory --network mumbai
 ```
---
+++ src/WillFactory.sol
@@ -0,0 +1,114 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WillFactory {
+
+    event NewWill(address, address);
+
+    function getCurrentTime() public view returns (uint256) {
+        return block.timestamp;
+    }
+
+    function buildWill(address willUser, uint256 expiration) public payable {
+        Will w = new Will(willUser, expiration);
+        console.log(address(w));
+        emit NewWill(willUser, address(w));
+    }
+}
+
+contract Will {
+    address public willUser;
+    address public Dao = 0x0000000000000000000000000000000000000001;
+    uint256 public expiration;
+    address[] private _guardians;
+    
+    struct guardianData {
+        bool isGuardian; 
+    }
+
+    mapping(address => guardianData) public guardianInfo;
+
+    modifier onlyOwner() {
+        require (msg.sender == willUser, "Unauthorized");
+        _;
+    }
+
+    modifier expiredCheck() {
+        require (isExpired() == false, "Already Expired");
+        _;
+    }
+
+    constructor(address _willUser, uint256 _expiration) {
+        willUser = _willUser;
+        expiration = block.timestamp + _expiration; // @TODO refactor -manny
+    }
+
+    // Check if time is over.
+    function isExpired() public view returns (bool) {
+        return block.timestamp >= expiration;
+    }
+
+    // Set more time.
+    function setExtension(uint256 _extension) external onlyOwner expiredCheck {
+        expiration = block.timestamp + timeLeft() + _extension;
+    }
+    
+    // How much time left.
+    function timeLeft() public view onlyOwner returns (uint256) {
+        return expiration - block.timestamp;
+    }
+
+    // Set guardians who will recieve the will after expiration.
+    function setGuardian(address _guardian) external onlyOwner expiredCheck {
+        guardianData storage _guardianA = guardianInfo[_guardian];
+        _guardians.push(_guardian);
+        _guardianA.isGuardian = true;
+    }
+
+    // Check the guardians.
+    function getGuardians() external view onlyOwner returns (address[] memory) {
+        return _guardians;
+    }
+
+    // Check the balance of the Will Owner
+    function getBalance() public view returns (uint) {
+        console.log("contract balance:", address(willUser).balance);
+        return address(willUser).balance;
+    }
+
+    // ***
+    function ownershipTransfer(address _guardian, address superGuardian) external {
+        guardianData storage _guardianA = guardianInfo[_guardian];
+        require (isExpired() == true, "Not Expired");
+        require(_guardianA.isGuardian, "Not Guardian");
+        superGuardian = _guardian;
+        finalizeWill(superGuardian);
+    }
+
+    // ***
+    function finalizeWill(address superGuardian) private {
+//        require (isExpired() == true, "Not Expired");
+        require (msg.sender == superGuardian, "Not guardian");
+        uint commissionAmount = address(willUser).balance * 2 / 100;
+        payCommission(commissionAmount);
+        sendAssets(superGuardian);
+        getBalance();
+    }
+
+    // 
+    function sendAssets(address superGuardian) private {
+        console.log("Sending willUser balance to guardians:", address(willUser).balance);
+        (bool sent, bytes memory data) = superGuardian.call{value: address(willUser).balance}("");
+        require(sent, "Failed to send Ether");
+        getBalance();
+    }
+
+    function payCommission(uint commissionAmount) private {     
+        console.log("Paying the commission of amount %s ", commissionAmount);
+        (bool sent, bytes memory data) = Dao.call{value: commissionAmount}("");
+        require(sent, "Failed to send Ether");      
+        getBalance(); 
+    }
+}
